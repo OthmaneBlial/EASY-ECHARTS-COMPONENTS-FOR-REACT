5d7fb656354433202d9c96af4b464221585ef07a
BLIAL OTHMANE <blial.othmane@gmail.com>
2019-03-09 19:01:19 +0000

    fix linting

--- src/partials/EchartsLine.js
+++ src/partials/EchartsLine.js
@@ -6,276 +6,263 @@ import elementResizeEvent from 'element-resize-event';
 
 class EchartsLine extends Component {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
 
-    this.state = {
-      test: 'Hi',
-      option : {
-          backgroundColor: this.props[this.props.theme],
-          title: {
-              text: this.props.title,
-              x: this.props.titlePosition,
-              top: 5,
-             textStyle: {
-                 fontStyle: this.props.titleFontStyle,
-                 fontFamily: this.props.fontFamily,
-                 fontWeight: this.props.titleFontWeight,
-                 fontSize: this.props.titleFontSize,
-             },
-          },
-          tooltip : {
-              trigger: 'axis',
-              axisPointer: {
-                  type: this.props.tooltipType,
-                  label: {
-                      backgroundColor: this.props.tooltipBackgroundColor
-                  }
-              }
-          },
-          legend: {
-              data:this.props.seriesNames,
-              top: 37,
-              textStyle: {
-                color: this.props.legendColor,
-                fontSize: this.props.legendFontSize,
-                fontFamily: this.props.fontFamily,
-              },
-          },
-          dataZoom: {
-            show: this.props.dataZoom,
-          },
-          toolbox: {
-              feature: {
-                  saveAsImage : {show: this.props.saveAsImage, title: 'SAVE'}
-              }
-          },
-          grid: {
-              left: this.props.gridLeft,
-              right: this.props.gridRight,
-              bottom: this.props.gridBottom,
-              containLabel: this.props.gridContainLabel
-          },
-          xAxis : [
-              {
-                  type : 'category',
-                  boundaryGap : ((this.props.seriesType === 'bar' || this.props.seriesType === 'barStack') ? true : false),
-                  data : this.props.xAxis,
-                  name: this.props.xAxisName,
-                  nameLocation: this.props.xAxisNameLocation,
-                  nameGap: this.props.xAxisNameGap
-              }
-          ],
-          yAxis : [
-              {
-                  type : 'value'
-              }
-          ],
-          series : this.switchOnEchartsLineType(this.props.seriesData)
-      }
-    }
-
-  }
-
-  switchOnEchartsLineType = (seriesData) => {
-   switch (this.props.seriesType) {
-    case 'areaStack':
-      return this.props.seriesNames.map((seriesName, index) =>
-
-               Object.assign({name: seriesName, type: 'line', stack: 'stackConnector', areaStyle: {normal: {}}, data: seriesData[index]})
-            )
-      break;
-    case 'area':
-      return this.props.seriesNames.map((seriesName, index) =>
-
-               Object.assign({name: seriesName, type: 'line', areaStyle: {normal: {}}, data: seriesData[index]})
-            )
-      break;
-      case 'line':
-        return this.props.seriesNames.map((seriesName, index) =>
-
-               Object.assign({name: seriesName, type: 'line', data: seriesData[index]})
-            )
-        break;
-      case 'lineStack':
-        return this.props.seriesNames.map((seriesName, index) =>
-
-               Object.assign({name: seriesName, type: 'line', stack: 'stackConnector', data: seriesData[index]})
-            )
-        break;
-      case 'bar':
-        return this.props.seriesNames.map((seriesName, index) =>
-
-               Object.assign({name: seriesName, type: 'bar', data: seriesData[index]})
-            )
-        break;
-      case 'barStack':
-        return this.props.seriesNames.map((seriesName, index) =>
-
-               Object.assign({name: seriesName, type: 'bar', stack: 'stackConnector', data: seriesData[index]})
-            )
-        break;
-
-      default:
-        return this.props.seriesNames.map((seriesName, index) =>
-
-               Object.assign({name: seriesName, type: 'line', data: seriesData[index]})
-            )
-        break;
+        this.state = {
+            test: 'Hi',
+            option: {
+                backgroundColor: this.props[this.props.theme],
+                title: {
+                    text: this.props.title,
+                    x: this.props.titlePosition,
+                    top: 5,
+                    textStyle: {
+                        fontStyle: this.props.titleFontStyle,
+                        fontFamily: this.props.fontFamily,
+                        fontWeight: this.props.titleFontWeight,
+                        fontSize: this.props.titleFontSize,
+                    },
+                },
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: this.props.tooltipType,
+                        label: {
+                            backgroundColor: this.props.tooltipBackgroundColor
+                        }
+                    }
+                },
+                legend: {
+                    data: this.props.seriesNames,
+                    top: 37,
+                    textStyle: {
+                        color: this.props.legendColor,
+                        fontSize: this.props.legendFontSize,
+                        fontFamily: this.props.fontFamily,
+                    },
+                },
+                dataZoom: {
+                    show: this.props.dataZoom,
+                },
+                toolbox: {
+                    feature: {
+                        saveAsImage: { show: this.props.saveAsImage, title: 'SAVE' }
+                    }
+                },
+                grid: {
+                    left: this.props.gridLeft,
+                    right: this.props.gridRight,
+                    bottom: this.props.gridBottom,
+                    containLabel: this.props.gridContainLabel
+                },
+                xAxis: [
+                    {
+                        type: 'category',
+                        boundaryGap: ((this.props.seriesType === 'bar' || this.props.seriesType === 'barStack') ? true : false),
+                        data: this.props.xAxis,
+                        name: this.props.xAxisName,
+                        nameLocation: this.props.xAxisNameLocation,
+                        nameGap: this.props.xAxisNameGap
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value'
+                    }
+                ],
+                series: this.switchOnEchartsLineType(this.props.seriesData)
+            }
+        }
 
     }
-  }
+
+    switchOnEchartsLineType = (seriesData) => {
+        switch (this.props.seriesType) {
+            case 'areaStack':
+                return this.props.seriesNames.map((seriesName, index) =>
+                    Object.assign({ name: seriesName, type: 'line', stack: 'stackConnector', areaStyle: { normal: {} }, data: seriesData[index] })
+                )
+                break;
+            case 'area':
+                return this.props.seriesNames.map((seriesName, index) =>
+
+                    Object.assign({ name: seriesName, type: 'line', areaStyle: { normal: {} }, data: seriesData[index] })
+                )
+                break;
+            case 'line':
+                return this.props.seriesNames.map((seriesName, index) =>
+                    Object.assign({ name: seriesName, type: 'line', data: seriesData[index] })
+                )
+                break;
+            case 'lineStack':
+                return this.props.seriesNames.map((seriesName, index) =>
+                    Object.assign({ name: seriesName, type: 'line', stack: 'stackConnector', data: seriesData[index] })
+                )
+                break;
+            case 'bar':
+                return this.props.seriesNames.map((seriesName, index) =>
+                    Object.assign({ name: seriesName, type: 'bar', data: seriesData[index] })
+                )
+                break;
+            case 'barStack':
+                return this.props.seriesNames.map((seriesName, index) =>
+                    Object.assign({ name: seriesName, type: 'bar', stack: 'stackConnector', data: seriesData[index] })
+                )
+                break;
+
+            default:
+                return this.props.seriesNames.map((seriesName, index) =>
+                    Object.assign({ name: seriesName, type: 'line', data: seriesData[index] })
+                )
+                break;
+
+        }
+    }
 
 
     renderChart = () => {
-
-      const chartDom = this.chart
-
-      let chart = echarts.getInstanceByDom(chartDom)
-
-      var myChart = echarts.init(chartDom, 'default');
-
-      myChart.setOption(this.state.option, true, true);
-
-      elementResizeEvent(chartDom, function() {
-
-        myChart.resize();
-
-      })
+        const chartDom = this.chart
+        const chart = echarts.getInstanceByDom(chartDom)
+        const myChart = echarts.init(chartDom, 'default');
+        myChart.setOption(this.state.option, true, true);
+        elementResizeEvent(chartDom, function() {
+            myChart.resize();
+        })
     }
 
 
     componentDidMount() {
-      this.renderChart();
+        this.renderChart();
     }
 
     componentWillReceiveProps(nextProps) {
-      if(this.props != nextProps) {
-        this.setState(prevState => ({
-          option: {
-          ...prevState.option,
-          series: this.switchOnEchartsLineType(nextProps.seriesData)
+        if (this.props != nextProps) {
+            this.setState(prevState => ({
+                option: {
+                    ...prevState.option,
+                    series: this.switchOnEchartsLineType(nextProps.seriesData)
+                }
+            }))
         }
-      }))
     }
-  }
 
     componentDidUpdate() {
-      this.renderChart();
+        this.renderChart();
     }
 
-  render() {
-    console.log(this.props);
-    console.log(this.state.option.series[0].data);
-    return (
-      <div  ref={chart => this.chart = chart} style={this.props.style} className={this.props.className} >
-      </div>
-    );
-  }
+    render() {
+        console.log(this.props);
+        console.log(this.state.option.series[0].data);
+        return (
+            <div ref={chart => this.chart = chart} style={this.props.style} className={this.props.className} >
+            </div>
+        );
+    }
 }
 
 
 EchartsLine.propTypes = {
-  // general propTypes
-  style: PropTypes.object,
-  className: PropTypes.string,
-  fontFamily: PropTypes.string,
-  themeOne: PropTypes.string,
-  themeTwo: PropTypes.string,
-  themeThree: PropTypes.string,
-  theme: PropTypes.string,
+    // general propTypes
+    style: PropTypes.object,
+    className: PropTypes.string,
+    fontFamily: PropTypes.string,
+    themeOne: PropTypes.string,
+    themeTwo: PropTypes.string,
+    themeThree: PropTypes.string,
+    theme: PropTypes.string,
 
-  // title propTypes
-  title: PropTypes.string,
-  titlePosition: PropTypes.string,
-  titleFontSize: PropTypes.string,
-  titleFontStyle: PropTypes.string,
-  titleFontWeight: PropTypes.string,
+    // title propTypes
+    title: PropTypes.string,
+    titlePosition: PropTypes.string,
+    titleFontSize: PropTypes.string,
+    titleFontStyle: PropTypes.string,
+    titleFontWeight: PropTypes.string,
 
-  // tooltip propTypes
-  tooltipType: PropTypes.string,
-  tooltipBackgroundColor: PropTypes.string,
+    // tooltip propTypes
+    tooltipType: PropTypes.string,
+    tooltipBackgroundColor: PropTypes.string,
 
-  // legend propTypes
-  legendColor: PropTypes.string,
-  legendFontSize: PropTypes.string,
+    // legend propTypes
+    legendColor: PropTypes.string,
+    legendFontSize: PropTypes.string,
 
-  // dataZoom propTypes
-  dataZoom: PropTypes.bool,
+    // dataZoom propTypes
+    dataZoom: PropTypes.bool,
 
-  // toolbox propTypes
-  saveAsImage: PropTypes.bool,
+    // toolbox propTypes
+    saveAsImage: PropTypes.bool,
 
-  // grid propTypes
-  gridLeft: PropTypes.string,
-  gridRight: PropTypes.string,
-  gridBottom: PropTypes.string,
-  gridContainLabel: PropTypes.bool,
+    // grid propTypes
+    gridLeft: PropTypes.string,
+    gridRight: PropTypes.string,
+    gridBottom: PropTypes.string,
+    gridContainLabel: PropTypes.bool,
 
-  // xAxis propTypes
-  xAxis: PropTypes.array,
-  xAxisName: PropTypes.string,
-  xAxisNameLocation: PropTypes.string,
-  xAxisNameGap: PropTypes.number,
+    // xAxis propTypes
+    xAxis: PropTypes.array,
+    xAxisName: PropTypes.string,
+    xAxisNameLocation: PropTypes.string,
+    xAxisNameGap: PropTypes.number,
 
-  // series propTypes
-  seriesType: PropTypes.string,
-  seriesNames: PropTypes.array,
-  seriesData: PropTypes.array,
+    // series propTypes
+    seriesType: PropTypes.string,
+    seriesNames: PropTypes.array,
+    seriesData: PropTypes.array,
 
 }
 
 EchartsLine.defaultProps = {
-  // general defaultProps
-  style: {
-    width: '100%',
-    height: '100vh',
-  },
-  className: '',
-  fontFamily: 'sans-serif',
-  themeOne: '#c9dbe2',
-  themeTwo: '#e5edff',
-  themeThree: '#f0ffe5',
-  theme: '#fff',
+    // general defaultProps
+    style: {
+        width: '100%',
+        height: '100vh',
+    },
+    className: '',
+    fontFamily: 'sans-serif',
+    themeOne: '#c9dbe2',
+    themeTwo: '#e5edff',
+    themeThree: '#f0ffe5',
+    theme: '#fff',
 
-  // title defaultProps
-  title: '',
-  titleFontSize: '15',
-  titlePosition: 'center',
-  titleFontStyle: 'normal',
-  titleFontWeight: 'bold',
+    // title defaultProps
+    title: '',
+    titleFontSize: '15',
+    titlePosition: 'center',
+    titleFontStyle: 'normal',
+    titleFontWeight: 'bold',
 
-  // tooltip defaultProps
-  tooltipType: 'cross',
-  tooltipBackgroundColor: '#b7aeaa',
+    // tooltip defaultProps
+    tooltipType: 'cross',
+    tooltipBackgroundColor: '#b7aeaa',
 
-  // legend defaultProps
-  legendColor: '#000',
-  legendFontSize: '11',
+    // legend defaultProps
+    legendColor: '#000',
+    legendFontSize: '11',
 
-  // dataZoom defaultProps
-  dataZoom: true,
+    // dataZoom defaultProps
+    dataZoom: true,
 
-  // toolbox defaultProps
-  saveAsImage: false,
+    // toolbox defaultProps
+    saveAsImage: false,
 
-  // grid defaultProps
-  gridLeft: '6%',
-  gridRight: '6%',
-  gridBottom: '14%',
-  gridContainLabel: true,
+    // grid defaultProps
+    gridLeft: '6%',
+    gridRight: '6%',
+    gridBottom: '14%',
+    gridContainLabel: true,
 
-  // xAxis defaultProps
-  xAxis: [],
-  xAxisName: '',
-  xAxisNameLocation: 'middle',
-  xAxisNameGap: 32,
+    // xAxis defaultProps
+    xAxis: [],
+    xAxisName: '',
+    xAxisNameLocation: 'middle',
+    xAxisNameGap: 32,
 
-  // series defaultProps
-  seriesType: 'line',
-  seriesNames: [],
-  seriesData: [],
+    // series defaultProps
+    seriesType: 'line',
+    seriesNames: [],
+    seriesData: [],
 
 
 }
--- src/partials/EchartsPie.js
+++ src/partials/EchartsPie.js
@@ -6,170 +6,160 @@ import elementResizeEvent from 'element-resize-event';
 
 class EchartsPie extends Component {
 
-  constructor(props) {
-    super(props);
-
-
-    this.state = {
-      option : {
-          backgroundColor: this.props[this.props.theme],
-          title: {
-              text: this.props.title,
-              x: this.props.titlePosition,
-              top: 30,
-             textStyle: {
-                 fontStyle: this.props.titleFontStyle,
-                 fontFamily: this.props.fontFamily,
-                 fontWeight: this.props.titleFontWeight,
-                 fontSize: this.props.titleFontSize,
-             },
-          },
-          tooltip : {
-            trigger: 'item',
-            formatter: "{a} <br/>{b} : {c} ({d}%)"
-          },
-          legend: {
-            orient: 'horizontal',
-            left: 'center',
-            top: 63,
-            data: this.props.legend
-          },
-          toolbox: {
-              feature: {
-                  saveAsImage : {show: this.props.saveAsImage, title: 'SAVE'}
-              }
-          },
-          series : [
-            {
-                name: 'Imported Fruits',
-                type: 'pie',
-                radius: this.props.doughnut ? ['50%', '60%'] : '50%',
-                avoidLabelOverlap: false,
-                center: ['50%', '55%'],
-                data: this.props.data,
-                itemStyle: {
-                    emphasis: {
-                        shadowBlur: 10,
-                        shadowOffsetX: 0,
-                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+    constructor(props) {
+        super(props);
+        this.state = {
+            option: {
+                backgroundColor: this.props[this.props.theme],
+                title: {
+                    text: this.props.title,
+                    x: this.props.titlePosition,
+                    top: 30,
+                    textStyle: {
+                        fontStyle: this.props.titleFontStyle,
+                        fontFamily: this.props.fontFamily,
+                        fontWeight: this.props.titleFontWeight,
+                        fontSize: this.props.titleFontSize,
+                    },
+                },
+                tooltip: {
+                    trigger: 'item',
+                    formatter: "{a} <br/>{b} : {c} ({d}%)"
+                },
+                legend: {
+                    orient: 'horizontal',
+                    left: 'center',
+                    top: 63,
+                    data: this.props.legend
+                },
+                toolbox: {
+                    feature: {
+                        saveAsImage: { show: this.props.saveAsImage, title: 'SAVE' }
                     }
-                }
+                },
+                series: [
+                    {
+                        name: 'Imported Fruits',
+                        type: 'pie',
+                        radius: this.props.doughnut ? ['50%', '60%'] : '50%',
+                        avoidLabelOverlap: false,
+                        center: ['50%', '55%'],
+                        data: this.props.data,
+                        itemStyle: {
+                            emphasis: {
+                                shadowBlur: 10,
+                                shadowOffsetX: 0,
+                                shadowColor: 'rgba(0, 0, 0, 0.5)'
+                            }
+                        }
+                    }
+                ]
             }
-        ]
-      }
+        }
+
     }
-
-  }
-
-
 
 
     renderChart = () => {
-
-      const chartDom = this.chart
-      let chart = echarts.getInstanceByDom(chartDom)
-
-      var myChart = echarts.init(chartDom, 'default');
-
-      if (this.state.option && typeof this.state.option === "object") {
-          myChart.setOption(this.state.option, false, false);
-      }
-
-      elementResizeEvent(chartDom, function() {
-
-        myChart.resize();
-
-      })
+        const chartDom = this.chart
+        const chart = echarts.getInstanceByDom(chartDom)
+        const myChart = echarts.init(chartDom, 'default');
+        if (this.state.option && typeof this.state.option === "object") {
+            myChart.setOption(this.state.option, false, false);
+        }
+        elementResizeEvent(chartDom, function() {
+            myChart.resize();
+        })
     }
 
 
     componentDidMount() {
-      this.renderChart();
+        this.renderChart();
     }
 
     componentWillUpdate() {
-      this.renderChart();
+        this.renderChart();
     }
 
-  render() {
+    render() {
 
-    return (
-      <div  ref={chart => this.chart = chart} style={this.props.style} className={this.props.className} >
-      </div>
-    );
-  }
+        return (
+            <div ref={chart => this.chart = chart} style={this.props.style} className={this.props.className} >
+            </div>
+        );
+    }
 }
 
 
 EchartsPie.propTypes = {
-  // general propTypes
-  style: PropTypes.object,
-  className: PropTypes.string,
-  fontFamily: PropTypes.string,
-  themeOne: PropTypes.string,
-  themeTwo: PropTypes.string,
-  themeThree: PropTypes.string,
-  theme: PropTypes.string,
+    // general propTypes
+    style: PropTypes.object,
+    className: PropTypes.string,
+    fontFamily: PropTypes.string,
+    themeOne: PropTypes.string,
+    themeTwo: PropTypes.string,
+    themeThree: PropTypes.string,
+    theme: PropTypes.string,
 
-  // title propTypes
-  title: PropTypes.string,
-  titlePosition: PropTypes.string,
-  titleFontSize: PropTypes.string,
-  titleFontStyle: PropTypes.string,
-  titleFontWeight: PropTypes.string,
+    // title propTypes
+    title: PropTypes.string,
+    titlePosition: PropTypes.string,
+    titleFontSize: PropTypes.string,
+    titleFontStyle: PropTypes.string,
+    titleFontWeight: PropTypes.string,
 
-  // tooltip propTypes
-  tooltipType: PropTypes.string,
-  tooltipBackgroundColor: PropTypes.string,
+    // tooltip propTypes
+    tooltipType: PropTypes.string,
+    tooltipBackgroundColor: PropTypes.string,
 
-  // legend propTypes
-  legendColor: PropTypes.string,
-  legendFontSize: PropTypes.string,
+    // legend propTypes
+    legendColor: PropTypes.string,
+    legendFontSize: PropTypes.string,
 
 
-  // toolbox propTypes
-  saveAsImage: PropTypes.bool,
+    // toolbox propTypes
+    saveAsImage: PropTypes.bool,
 
-  // series propTypes
-  doughnut: PropTypes.bool,
-  legend: PropTypes.array,
-  data: PropTypes.array,
+    // series propTypes
+    doughnut: PropTypes.bool,
+    legend: PropTypes.array,
+    data: PropTypes.array,
 
 }
 
 EchartsPie.defaultProps = {
-  // general defaultProps
-  style: {
-    width: '100%',
-    height: '100vh',
-  },
-  className: '',
-  fontFamily: 'sans-serif',
-  themeOne: '#c9dbe2',
-  themeTwo: '#e5edff',
-  themeThree: '#f0ffe5',
-  theme: '#fff',
+    // general defaultProps
+    style: {
+        width: '100%',
+        height: '100vh',
+    },
+    className: '',
+    fontFamily: 'sans-serif',
+    themeOne: '#c9dbe2',
+    themeTwo: '#e5edff',
+    themeThree: '#f0ffe5',
+    theme: '#fff',
 
-  // title defaultProps
-  title: '',
-  titleFontSize: '18',
-  titlePosition: 'center',
-  titleFontStyle: 'normal',
-  titleFontWeight: 'bold',
-
-
-  // legend defaultProps
-  legendColor: '#000',
-  legendFontSize: '11',
-
-  // toolbox defaultProps
-  saveAsImage: false,
+    // title defaultProps
+    title: '',
+    titleFontSize: '18',
+    titlePosition: 'center',
+    titleFontStyle: 'normal',
+    titleFontWeight: 'bold',
 
 
-  // series defaultProps
-  doughnut: false,
-  legend: [],
-  data: [],
+    // legend defaultProps
+    legendColor: '#000',
+    legendFontSize: '11',
+
+    // toolbox defaultProps
+    saveAsImage: false,
+
+
+    // series defaultProps
+    doughnut: false,
+    legend: [],
+    data: [],
 
 
 }
